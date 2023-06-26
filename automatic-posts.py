bce4774c2a33d426d8268c9320eef46715e3b89a
protoblaster <120064629+protoblaster@users.noreply.github.com>
2023-06-26 21:01:28 +0100

    commit v2 (refactored code)

--- automatic-posts.py
+++ automatic-posts.py
@@ -1,87 +1,57 @@
 import os
 import datetime
 
-author = None
-article_title = None
-date = datetime.date.today()
-date = date.strftime('%Y-%m-%d')
-timezone = None
-article_date = None
-layout = None
-file_name = None
-path_name = None
-dashes = '---'
-categories = []
-
-variable_names = []
-variable_values = []
-
 def read_input():
-    
-    os.system('clear')
-
-    global author, article_title, date, timezone, article_date, layout, file_name
-    global path_name, variable_names, variable_values, categories
-
-    author = input('Enter the author of the article> ')
-    article_title = input('Enter the title of the article> ')
-    timezone = input('Enter the timezone> ')
-    article_date = date + ' ' + timezone
+    author = input('Enter the author\'s name> ')
+    article_title = input('Enter the title for the aricle> ')
+    timezone = input('Enter the timezone (London = +0100)> ')
+    date = datetime.date.today()
+    article_date = datetime.date.today().strftime('%Y-%m-%d') + ' ' + timezone
     layout = input('Enter the layout for the page> ')
     file_name = input('Enter the file name> ')
-    path_name = input('Enter the path to store the new file> ')
+    path_name = input('Enter the path to store the file in> ')
 
-    variable_names = ['Author:', 'Article Date:', 'Layout:', 'Path to file:',
-                      'Article Title:', 'Categories:']
-    variable_values = [author, article_date, layout, path_name + file_name,
-                       article_title, categories]
-
-    for index in range(3):
-        tag = input('Enter a tag for the blog post> ')
+    categories = []
+    for _ in range(3):
+        tag = input('Enter a tag for the article> ')
         categories.append(tag)
 
-def check_input():
-    
-    global variable_names, variable_values
+    return author, article_title, article_date, layout, file_name, path_name, categories, date
 
+def check_input(variable_names, variable_values):
     os.system('clear')
+    for name, value in zip(variable_names, variable_values):
+        print(f'{name} {value}')
+    print()
 
-    for index, x in enumerate(variable_names):
-        print(variable_names[index], variable_values[index])
-
-    print('\n')
-
-
-def write_file():
-    
-    global author, article_title, article_date, layout, file_name, path_name
-    global categories
-
+def write_file(author, article_title, article_date, layout, file_name, path_name, categories, date):
     try:
-        with open(f'{path_name}/{file_name}', 'w') as file:
-            file.write(f'{dashes} \n')
-            file.write(f'author: {author} \n')
-            file.write(f'title: {article_title} \n')
-            file.write(f'date: {article_date} \n')
-            file.write(f'layout: {layout} \n')
-            file.write('categories: ')
+        file_path = os.path.join(path_name, f'{date}-{file_name}')
 
-            for index, x in enumerate(categories):
-                file.write(x + ' ')
+        with open(file_path, 'w') as file:
+            file.write('---\n')
+            file.write(f'author: {author}\n')
+            file.write(f'title: {article_title}\n')
+            file.write(f'date: {article_date}\n')
+            file.write(f'layout: {layout}\n')
+            file.write(f'categories: ' + ' '.join(categories) + '\n')
+            file.write('---\n')
 
-            file.write(f'\n{dashes} \n')
+    except OSError:
+        print('The Directory to store the file doesn\'t seem to exist!')
+        path_name = input('Enter the path name only> ')
+        file_name = input('Enter the file name only> ')
+        write_file(author, article_title, article_date, layout, file_name, path_name, categories, date)
 
-    except OSError as e:
-        print('The Directory to store the file in does not exist.',
-              '\n', e)
-        path_name = input('Enter the path name> ')
-        file_name = input('Enter the file name> ')
-        
-        write_file()
-    
     else:
-        print('The program has been successfully executed')
+        print('The program executed successfully!')
 
-read_input()
-check_input()
-write_file()
+def main():
+    input_values = read_input()
+    variable_names = ['Author:', 'Article Title:', 'Article Date:', 'Layout:', 'File Name:', 'Categories:']
+
+    check_input(variable_names, input_values)
+    write_file(*input_values)
+
+if __name__ == '__main__':
+    main()
